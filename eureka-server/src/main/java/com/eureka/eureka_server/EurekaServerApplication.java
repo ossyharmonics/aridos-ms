bcea01962732111129cb8925f5010887f84a8ee0
ossyharmonics <oss.herrera@duocuc.cl>
2026-07-02 21:13:07 -0400

    Se modifica el .properties de eureka server.

--- eureka-server/src/main/java/com/eureka/eureka_server/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/eureka/eureka_server/EurekaServerApplication.java
@@ -1,13 +1,20 @@
 package com.eureka.eureka_server;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerApplication.class, args);
+		SpringApplication app = new SpringApplication(EurekaServerApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("server.port", "8761"));
+        System.setProperty("eureka.client.register-with-eureka", "false");
+		System.setProperty("eureka.client.fetch-registry","false");
 	}
 
 }
